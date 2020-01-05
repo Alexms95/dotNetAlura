863ac8fe47172db6face5d9e76c87105993873b9
Alexms95 <silva.alexmatos@gmail.com>
2020-01-05 19:57:30 -0300

    Criando CRUD com Entity

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,29 +11,78 @@ namespace Alura.Loja.Testes.ConsoleApp
         static void Main(string[] args)
         {
             //GravarUsandoAdoNet();
+            //GravarUsandoEntity();
+            //RecuperarProdutos();
+            //ExcluirProdutos();
+            //RecuperarProdutos();
+            AtualizarProduto();
+        }
+
+        private static void AtualizarProduto()
+        {
+            //Incluir um produto
             GravarUsandoEntity();
+            RecuperarProdutos();
+
+            //Atualizar o produto
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                Produto primeiro = contexto.Produtos().First();
+                primeiro.Nome = "Harry Potter Editado";
+                contexto.Atualizar(primeiro);
+            }
+            RecuperarProdutos();
+
+        }
+
+        private static void ExcluirProdutos()
+        {
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = contexto.Produtos();
+                foreach (var produto in produtos)
+                {
+                    contexto.Remover(produto);
+                }
+            }
+        }
+
+        private static void RecuperarProdutos()
+        {
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = contexto.Produtos();
+                Console.WriteLine("Foram recuperados {0} produtos.", produtos.Count);
+                foreach (var produto in produtos)
+                {
+                    Console.WriteLine(produto.Nome);
+                }
+            }
         }
 
         private static void GravarUsandoEntity()
         {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var contexto = new LojaContext())
+            Produto p = new Produto
             {
-                contexto.Produtos.Add(p);
-                contexto.SaveChanges();
+                Nome = "Harry Potter e a Ordem da Fênix",
+                Categoria = "Livros",
+                Preco = 19.89
+            };
+
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                contexto.Adicionar(p);
             }
         }
 
         private static void GravarUsandoAdoNet()
         {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
+            Produto p = new Produto
+            {
+                Nome = "Harry Potter e a Ordem da Fênix",
+                Categoria = "Livros",
+                Preco = 19.89
+            };
 
             using (var repo = new ProdutoDAO())
             {
