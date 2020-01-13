a76d06d88b6cd7d686e9105b926d8ffed1f68a75
Alexms95 <silva.alexmatos@gmail.com>
2020-01-13 19:32:36 -0300

    Relacionamento 1 x n e n x n

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,7 +10,43 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-           
+            var p1 = new Produto 
+            {
+                Nome = "Suco de Laranja",
+                Categoria = "Bebidas",
+                PrecoUnitario = 8.79,
+                Unidade = "Litros"
+            };
+            var p2 = new Produto
+            {
+                Nome = "Café",
+                Categoria = "Bebidas",
+                PrecoUnitario = 12.45,
+                Unidade = "Gramas"
+            };
+            var p3 = new Produto 
+            {
+                Nome = "Macarrão",
+                Categoria = "Alimentos",
+                PrecoUnitario = 4.23,
+                Unidade = "Gramas"
+            };
+
+            var promocaoDePascoa = new Promocao
+            {
+                Descricao = "Páscoa Feliz",
+                DataInicio = DateTime.Now,
+                DataTermino = DateTime.Now.AddMonths(3),
+            };
+            promocaoDePascoa.IncluirProduto(p1);
+            promocaoDePascoa.IncluirProduto(p2);
+            promocaoDePascoa.IncluirProduto(p3);
+
+            using (var contexto = new LojaContext())
+            {
+                contexto.Promocoes.Add(promocaoDePascoa);
+                contexto.SaveChanges();
+            }
         }
     }
 }
