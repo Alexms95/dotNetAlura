e392a83ff78ac0c9f7a760eb4b97e269e12a0559
Alexms95 <silva.alexmatos@gmail.com>
2020-01-18 19:20:51 -0300

    Fim do curso de ef core

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,101 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var p1 = new Produto 
+            using (var contexto = new LojaContext())
+            {
+                var cliente = contexto
+                    .Clientes
+                    .Include(c => c.EnderecoDeEntrega)
+                    .FirstOrDefault();
+                Console.WriteLine($"Endereço de entrega: {cliente.EnderecoDeEntrega.Logradouro}");
+
+                var produto = contexto
+                    .Produtos
+                    .Where(p => p.Id == 1002)
+                    .FirstOrDefault();
+                Console.WriteLine($"Mostrando as compras do produto {produto.Nome}");
+
+                contexto
+                    .Entry(produto)
+                    .Collection(p => p.Compras)
+                    .Query()
+                    .Where(c => c.Preco > 10)
+                    .Load();
+
+                foreach (var item in produto.Compras)
+                {
+                    Console.WriteLine(item.Quantidade);
+                }
+            }
+        }
+
+        private static void ExibeProdutosDaPromoção()
+        {
+            using (var contexto2 = new LojaContext())
+            {
+                var primeiraPromocao = contexto2
+                    .Promocoes
+                    .Include(p => p.Produtos)
+                    .ThenInclude(pp => pp.Produto)
+                    .FirstOrDefault();
+
+                Console.WriteLine("\nMostrando os produtos da promoção...");
+
+                foreach (var item in primeiraPromocao.Produtos)
+                {
+                    Console.WriteLine(item.Produto);
+                }
+            }
+        }
+
+        private static void IncluirPromocao()
+        {
+            using (var contexto = new LojaContext())
+            {
+                var promocao = new Promocao
+                {
+                    Descricao = "Queima Total Janeiro 2020",
+                    DataInicio = new DateTime(2020, 1, 1),
+                    DataTermino = new DateTime(2020, 1, 31)
+                };
+
+                var produtos = contexto.Produtos.Where(p => p.Categoria == "Bebidas").ToList();
+
+                foreach (var item in produtos)
+                {
+                    promocao.IncluirProduto(item);
+                }
+
+                contexto.Add(promocao);
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void UmParaUm()
+        {
+            var fulano = new Cliente
+            {
+                Nome = "Fulaninho de Tal"
+            };
+
+            fulano.EnderecoDeEntrega = new Endereco
+            {
+                Numero = 12,
+                Logradouro = "Rua dos Inválidos",
+                Bairro = "Centro",
+                Cidade = "Cidade"
+            };
+
+            using (var contexto = new LojaContext())
+            {
+                contexto.Clientes.Add(fulano);
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void MuitosParaMuitos()
+        {
+            var p1 = new Produto
             {
                 Nome = "Suco de Laranja",
                 Categoria = "Bebidas",
@@ -24,7 +119,7 @@ namespace Alura.Loja.Testes.ConsoleApp
                 PrecoUnitario = 12.45,
                 Unidade = "Gramas"
             };
-            var p3 = new Produto 
+            var p3 = new Produto
             {
                 Nome = "Macarrão",
                 Categoria = "Alimentos",
