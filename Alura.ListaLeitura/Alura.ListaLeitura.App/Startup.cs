e33d09573ca94ce5e0f05600f58f35d76648fa4d
Alexms95 <silva.alexmatos@gmail.com>
2020-01-26 14:59:43 -0300

    Implementando o MVC e usando as Views

--- Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,4 @@
-﻿using Alura.ListaLeitura.App.Logica;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Alura.ListaLeitura.App
@@ -9,21 +7,13 @@ namespace Alura.ListaLeitura.App
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRouting();
+            services.AddMvc();
         }
+
         public void Configure(IApplicationBuilder app)
         {
-            RouteBuilder builder = new RouteBuilder(app);
-            builder.MapRoute("Livros/ParaLer", LivrosLogica.LivrosParaLer);
-            builder.MapRoute("Livros/Lendo", LivrosLogica.LivrosLendo);
-            builder.MapRoute("Livros/Lido", LivrosLogica.LivrosLidos);
-            builder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", CadastroLogica.NovoLivroParaLer);
-            builder.MapRoute("Livros/Detalhes/{id:int}", LivrosLogica.ExibirDetalhes);
-            builder.MapRoute("Cadastro/NovoLivro", CadastroLogica.ExibeFormulario);
-            builder.MapRoute("Cadastro/Incluir", CadastroLogica.ProcessaFormulario);
-            IRouter rotas = builder.Build();
-
-            app.UseRouter(rotas);
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
         } 
     }
 }
