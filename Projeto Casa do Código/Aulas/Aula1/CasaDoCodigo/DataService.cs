bcc5b4734667d7e0dba84951f0d0a399085fabd0
Alexms95 <silva.alexmatos@gmail.com>
2020-02-09 17:39:15 -0300

    Criando repositorio e adicionando produtos na view

---
+++ Projeto Casa do Código/Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -0,0 +1,40 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repository;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    class DataService : IDataService
+    {
+        private readonly ApplicationContext _contexto;
+        private readonly IProdutoRepository _produtoRepository;
+
+        public DataService(ApplicationContext contexto,
+            IProdutoRepository produtoRepository)
+        {
+            _contexto = contexto;
+            _produtoRepository = produtoRepository;
+        }
+
+        public void InicializaDB()
+        {
+            _contexto.Database.EnsureCreated();
+            List<Livro> livros = GetLivros();
+
+           _produtoRepository.SaveProdutos(livros);
+        }
+
+        
+
+        private static List<Livro> GetLivros()
+        {
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+    }
+
+    
+}
